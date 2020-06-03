8b7e2e6c31d2af4ee8ce18765e6b6f784439f079
Tim Shelburne <shelburt02@gmail.com>
2020-06-03 15:33:42 -0400

    fixed nesting at various levels

--- src/routing-driver.js
+++ src/routing-driver.js
@@ -155,7 +155,7 @@ export function routes(strings, ...paramsValues) {
 		}
 
 		if (prev.indent > current.indent) {
-			stack = stack.slice(0, prev.indent - current.indent - base)
+			stack = stack.slice(0, Math.max(prev.indent - current.indent - base, 0))
 		}
 
 		const parent =
