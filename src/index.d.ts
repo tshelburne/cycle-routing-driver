c182b413c5cb5aba0421b18a88443d46dfe9ade4
Christopher Reed-Asakawa <askerushiro@gmail.com>
2020-08-21 12:46:58 -0700

    added type safety to exported methods

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,26 @@
 declare module 'cycle-routing-driver' {
+	type Page = {
+		to: string
+	}
 	export type Route = {
 		page: string
 		data: { [key: string]: any }
 		query: { [key: string]: any }
 	}
+
+	export const Router: (args: {
+		route: Route,
+		children?: ReactNode[]
+	}) => React.Context
+	export const Route: (args: {
+		match: (string | function | object),
+		strict: boolean,
+		children?: ReactNode[]
+	}) => (ReactNode | null)
+	export const Link = <T = ReactNode>(args: {
+		to: (string | Page),
+		activeClassName: string,
+		className: string
+		props?: Props<T>
+	}) => ReactNode
 }
